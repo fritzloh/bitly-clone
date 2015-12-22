397c17ae9507893f3976bd1ccd27a7b4782057ee
fritzloh <fritzloh@gmail.com>
2015-12-22 17:18:23 +0800

    cahnge

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,20 @@
+
+require_relative '../models/url'
+
 get '/' do
+  @urls = Url.last
   erb :"static/index"
+end
+
+post '/urls' do 
+    x = Url.create(long_url: params["long_url"])
+    x.shorten
+    x.save
+    redirect to('/')
+end
+
+
+get '/:shortened' do
+  url = Url.find_by(short_url: params[:shortened])
+  redirect to "#{url.long_url}"
 end
