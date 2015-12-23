c7eafb51b9e2e2327a441ee8616dd0dd58445f85
fritzloh <fritzloh@gmail.com>
2015-12-23 16:44:29 +0800

    html css

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -2,19 +2,26 @@
 require_relative '../models/url'
 
 get '/' do
-  @urls = Url.last
+  @url = Url.last if Url.last
+  
   erb :"static/index"
 end
 
 post '/urls' do 
     x = Url.create(long_url: params["long_url"])
     x.shorten
-    x.save
+    if x.save
+     then
     redirect to('/')
+else
+	erb :"static/error"
+end
 end
 
 
 get '/:shortened' do
   url = Url.find_by(short_url: params[:shortened])
+  url.click_count += 1
+  url.save
   redirect to "#{url.long_url}"
 end
